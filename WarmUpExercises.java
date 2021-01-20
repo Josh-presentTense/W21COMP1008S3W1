31e32c75031d537959db11ef4ef74bfe006a137b
Josh-presentTense <200464522@student.georgianc.on.ca>
2021-01-20 17:39:12 -0500

    Initial commit

---
+++ WarmUpExercises.java
@@ -0,0 +1,10 @@
+public class WarmUpExercises{
+
+    public static void main(args[] in){
+        //loop that counts down by intervals of 5 from 100
+        for(int i = 100; i >= 0; i-5){
+            System.out.println(i);
+
+        }
+    }
+}
